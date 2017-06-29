38e57d8f91442f188bea17c1926e81e733dbd022
sereneblue <sereneblue@users.noreply.github.com>
2017-06-28 20:05:37 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ wget --header='Accept-Encoding:none' -O /tmp/krypto_extension.zip "${URL}"
 #Make directory and extract archive to it
 mkdir -p "${EXTENSION_PATH}/${EXTENSION_UUID}";
 unzip -q /tmp/krypto_extension.zip -d ${EXTENSION_PATH}/${EXTENSION_UUID};
-mv ${EXTENSION_PATH}/${EXTENSION_UUID}/gnome-shell-extension-krypto-master/krypto@sereneblue/* ${EXTENSION_PATH}/${EXTENSION_UUID};
+cp -r ${EXTENSION_PATH}/${EXTENSION_UUID}/gnome-shell-extension-krypto-master/krypto@sereneblue/* ${EXTENSION_PATH}/${EXTENSION_UUID}
 
 # Remove repo directory and extra files
-rm -r ignore-fail-on-non-empty ${EXTENSION_PATH}/${EXTENSION_UUID}/gnome-shell-extension-krypto-master;
+rm -r ${EXTENSION_PATH}/${EXTENSION_UUID}/gnome-shell-extension-krypto-master;
 
 # List enabled extensions
 EXTENSION_LIST=$(gsettings get org.gnome.shell enabled-extensions | sed 's/^.\(.*\).$/\1/');
@@ -24,7 +24,7 @@ EXTENSION_ENABLED=$(echo ${EXTENSION_LIST} | grep ${EXTENSION_UUID});
 if [ "$EXTENSION_ENABLED" = "" ]; then
 	gsettings set org.gnome.shell enabled-extensions "[${EXTENSION_LIST},'${EXTENSION_UUID}']"
 	# Extension is now available
-	echo "Extension with ID ${EXTENSION_ID} has been enabled. Restart your desktop to take effect (Alt+F2 then 'r')."
+	echo "krypto has been enabled. Restart your desktop to take effect (Alt+F2 then 'r')."
 fi
 
 # remove temporary files
