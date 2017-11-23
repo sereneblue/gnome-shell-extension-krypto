8bdfa50c0126960ec259efa9953b63a0ee190e1a
sereneblue <sereneblue@users.noreply.github.com>
2017-11-22 23:44:59 -0500

    add support for more currencies & remove some delimeters

--- krypto@sereneblue/constants.js
+++ krypto@sereneblue/constants.js
@@ -4,44 +4,69 @@ const UPDATE_SEC = "krypto-prefs-update";
 const DISTRACTION_MIN = "krypto-prefs-distraction";
 const NUM_DISPLAY = "krypto-prefs-num";
 const DELIMITER = "krypto-prefs-delim"
+const BCH_ENABLED = "krypto-prefs-bch";
 const BTC_ENABLED = "krypto-prefs-btc";
 const DASH_ENABLED = "krypto-prefs-dash";
 const DCR_ENABLED = "krypto-prefs-dcr";
 const DOGE_ENABLED = "krypto-prefs-doge";
 const ETH_ENABLED = "krypto-prefs-eth";
 const ETC_ENABLED = "krypto-prefs-etc";
+const IOTA_ENABLED = "krypto-prefs-iota";
+const LSK_ENABLED = "krypto-prefs-lsk";
 const LTC_ENABLED = "krypto-prefs-ltc";
+const NEO_ENABLED = "krypto-prefs-neo";
+const NXT_ENABLED = "krypto-prefs-nxt";
+const REP_ENABLED = "krypto-prefs-rep";
+const STRAT_ENABLED = "krypto-prefs-strat";
+const XEM_ENABLED = "krypto-prefs-xem";
 const XMR_ENABLED = "krypto-prefs-xmr";
 const XRP_ENABLED = "krypto-prefs-xrp";
+const ZEC_ENABLED = "krypto-prefs-zec";
 const CRYPTOS = [
+	"BCH",
 	"BTC",
 	"DASH",
 	"DCR",
 	"DOGE",
 	"ETH",
 	"ETC",
+	"IOTA",
+	"LSK",
 	"LTC",
+	"NEO",
+	"NXT",
+	"REP",
+	"STRAT",
+	"XEM",
 	"XMR",
-	"XRP"
+	"XRP",
+	"ZEC"
 ];
 const ENABLED_CRYPTOS = [
+	BCH_ENABLED,
 	BTC_ENABLED,
 	DASH_ENABLED,
 	DCR_ENABLED,
 	DOGE_ENABLED,
 	ETH_ENABLED,
 	ETC_ENABLED,
+	IOTA_ENABLED,
+	LSK_ENABLED,
 	LTC_ENABLED,
+	NEO_ENABLED,
+	NXT_ENABLED,
+	REP_ENABLED,
+	STRAT_ENABLED,
+	XEM_ENABLED,
 	XMR_ENABLED,
-	XRP_ENABLED
-]
+	XRP_ENABLED,
+	ZEC_ENABLED
+];
 const SYMBOLS = ["A$", "C$", " Fr.", "¥", "€",
-				  "£", "HK$", "₹", "¥",
-				  "Mex$", "NZ$", "₽", "S$", "$"];
+				"£", "HK$", "₹", "¥",
+				"Mex$", "NZ$", "₽", "S$", "$"];
 const FIAT_ABBR = ["AUD", "CAD", "CHF", "CNY",
-					"EUR", "GBP", "HKD", "INR",
-					"JPY", "MXN", "NZD", "RUB",
-					"SGD", "USD"];
-const DELIMS = ["|", "·", "•", "◈",
-				"✖", "†", "‡", "~"]
-
+				   "EUR", "GBP", "HKD", "INR",
+				   "JPY", "MXN", "NZD", "RUB",
+				   "SGD", "USD"];
+const DELIMS = ["|", "·", "•", "◈", "~"];
