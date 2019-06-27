6c4cb604dac6252821c05fb4628b11e3b22d2d5b
sereneblue <sereneblue@users.noreply.github.com>
2019-06-26 23:33:09 -0400

    updated supported cryptocurrencies

--- krypto@sereneblue/constants.js
+++ krypto@sereneblue/constants.js
@@ -4,62 +4,89 @@ const UPDATE_SEC = "krypto-prefs-update";
 const DISTRACTION_MIN = "krypto-prefs-distraction";
 const NUM_DISPLAY = "krypto-prefs-num";
 const DELIMITER = "krypto-prefs-delim"
+const ADA_ENABLED = "krypto-prefs-ada";
+const BAT_ENABLED = "krypto-prefs-bat";
 const BCH_ENABLED = "krypto-prefs-bch";
+const BNB_ENABLED = "krypto-prefs-bnb";
 const BTC_ENABLED = "krypto-prefs-btc";
 const DASH_ENABLED = "krypto-prefs-dash";
 const DCR_ENABLED = "krypto-prefs-dcr";
 const DOGE_ENABLED = "krypto-prefs-doge";
+const EOS_ENABLED = "krypto-prefs-eos";
 const ETH_ENABLED = "krypto-prefs-eth";
 const ETC_ENABLED = "krypto-prefs-etc";
 const IOTA_ENABLED = "krypto-prefs-iota";
+const LINK_ENABLED = "krypto-prefs-link";
 const LSK_ENABLED = "krypto-prefs-lsk";
 const LTC_ENABLED = "krypto-prefs-ltc";
+const MKR_ENABLED = "krypto-prefs-mkr";
+const NANO_ENABLED = "krypto-prefs-nano";
 const NEO_ENABLED = "krypto-prefs-neo";
 const NXT_ENABLED = "krypto-prefs-nxt";
 const REP_ENABLED = "krypto-prefs-rep";
 const STRAT_ENABLED = "krypto-prefs-strat";
+const VET_ENABLED = "krypto-prefs-vet";
 const XEM_ENABLED = "krypto-prefs-xem";
 const XMR_ENABLED = "krypto-prefs-xmr";
 const XRP_ENABLED = "krypto-prefs-xrp";
+const XTZ_ENABLED = "krypto-prefs-xtz";
 const ZEC_ENABLED = "krypto-prefs-zec";
 const CRYPTOS = [
+	"ADA",
+	"BAT",
 	"BCH",
+	"BNB",
 	"BTC",
 	"DASH",
 	"DCR",
 	"DOGE",
+	"EOS",
 	"ETH",
 	"ETC",
 	"IOTA",
+	"LINK",
 	"LSK",
 	"LTC",
+	"MKR",
+	"NANO",
 	"NEO",
 	"NXT",
 	"REP",
 	"STRAT",
+	"VET",
 	"XEM",
 	"XMR",
 	"XRP",
+	"XTZ",
 	"ZEC"
 ];
 const ENABLED_CRYPTOS = [
+	ADA_ENABLED,
+	BAT_ENABLED,
 	BCH_ENABLED,
+	BNB_ENABLED,
 	BTC_ENABLED,
 	DASH_ENABLED,
 	DCR_ENABLED,
 	DOGE_ENABLED,
+	EOS_ENABLED,
 	ETH_ENABLED,
 	ETC_ENABLED,
 	IOTA_ENABLED,
+	LINK_ENABLED,
 	LSK_ENABLED,
 	LTC_ENABLED,
+	MKR_ENABLED,
+	NANO_ENABLED,
 	NEO_ENABLED,
 	NXT_ENABLED,
 	REP_ENABLED,
 	STRAT_ENABLED,
+	VET_ENABLED,
 	XEM_ENABLED,
 	XMR_ENABLED,
 	XRP_ENABLED,
+	XTZ_ENABLED,
 	ZEC_ENABLED
 ];
 const SYMBOLS = ["A$", "C$", " Fr.", "¥", "€",
--- krypto@sereneblue/prefs.js
+++ krypto@sereneblue/prefs.js
@@ -55,23 +55,32 @@ const krypto_widget = new GObject.Class({
 	        let prefs_spin_update = builder.get_object("prefs_spin_update");
 	        let prefs_spin_distraction = builder.get_object("prefs_spin_distraction");
 	        let prefs_spin_num = builder.get_object("prefs_spin_num");
+	        let prefs_crypto_ada = builder.get_object("prefs_crypto_ada");
+	        let prefs_crypto_bat = builder.get_object("prefs_crypto_bat");
 	        let prefs_crypto_bch = builder.get_object("prefs_crypto_bch");
+	        let prefs_crypto_bnb = builder.get_object("prefs_crypto_bnb");
 	        let prefs_crypto_btc = builder.get_object("prefs_crypto_btc");
 	        let prefs_crypto_dash = builder.get_object("prefs_crypto_dash");
 	        let prefs_crypto_dcr = builder.get_object("prefs_crypto_dcr");
 	        let prefs_crypto_doge = builder.get_object("prefs_crypto_doge");
+	        let prefs_crypto_eos = builder.get_object("prefs_crypto_eos");
 	        let prefs_crypto_eth = builder.get_object("prefs_crypto_eth");
 	        let prefs_crypto_etc = builder.get_object("prefs_crypto_etc");
 	        let prefs_crypto_iota = builder.get_object("prefs_crypto_iota");
+	        let prefs_crypto_link = builder.get_object("prefs_crypto_link");
 	        let prefs_crypto_lsk = builder.get_object("prefs_crypto_lsk");
 	        let prefs_crypto_ltc = builder.get_object("prefs_crypto_ltc");
+			let prefs_crypto_mkr = builder.get_object("prefs_crypto_mkr");
+			let prefs_crypto_nano = builder.get_object("prefs_crypto_nano");
 	        let prefs_crypto_neo = builder.get_object("prefs_crypto_neo");
 	        let prefs_crypto_nxt = builder.get_object("prefs_crypto_nxt");
 	        let prefs_crypto_rep = builder.get_object("prefs_crypto_rep");
 	        let prefs_crypto_strat = builder.get_object("prefs_crypto_strat");
+	        let prefs_crypto_vet = builder.get_object("prefs_crypto_vet");
 	        let prefs_crypto_xem = builder.get_object("prefs_crypto_xem");
 	        let prefs_crypto_xmr = builder.get_object("prefs_crypto_xmr");
 	        let prefs_crypto_xrp = builder.get_object("prefs_crypto_xrp");
+	        let prefs_crypto_xtz = builder.get_object("prefs_crypto_xtz");
 	        let prefs_crypto_zec = builder.get_object("prefs_crypto_zec");
 
 	        // bind settings
@@ -80,23 +89,32 @@ const krypto_widget = new GObject.Class({
 	        this.settings.bind(Settings.DISTRACTION_MIN, prefs_spin_distraction, "value", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.NUM_DISPLAY, prefs_spin_num, "value", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.DELIMITER, prefs_combo_delim, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.ADA_ENABLED, prefs_crypto_ada, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.BAT_ENABLED, prefs_crypto_bat, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.BCH_ENABLED, prefs_crypto_bch, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.BNB_ENABLED, prefs_crypto_bnb, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.BTC_ENABLED, prefs_crypto_btc, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.DASH_ENABLED, prefs_crypto_dash, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.DCR_ENABLED, prefs_crypto_dcr, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.DOGE_ENABLED, prefs_crypto_doge, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.EOS_ENABLED, prefs_crypto_eos, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.ETH_ENABLED, prefs_crypto_eth, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.ETC_ENABLED, prefs_crypto_etc, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.IOTA_ENABLED, prefs_crypto_iota, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.LINK_ENABLED, prefs_crypto_link, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.LSK_ENABLED, prefs_crypto_lsk, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.LTC_ENABLED, prefs_crypto_ltc, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.MKR_ENABLED, prefs_crypto_mkr, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.NANO_ENABLED, prefs_crypto_nano, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.NEO_ENABLED, prefs_crypto_neo, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.NXT_ENABLED, prefs_crypto_nxt, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.REP_ENABLED, prefs_crypto_rep, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.STRAT_ENABLED, prefs_crypto_strat, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.VET_ENABLED, prefs_crypto_vet, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.XEM_ENABLED, prefs_crypto_xem, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.XMR_ENABLED, prefs_crypto_xmr, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.XRP_ENABLED, prefs_crypto_xrp, "active", Gio.SettingsBindFlags.DEFAULT);
+	        this.settings.bind(Settings.XTZ_ENABLED, prefs_crypto_xtz, "active", Gio.SettingsBindFlags.DEFAULT);
 	        this.settings.bind(Settings.ZEC_ENABLED, prefs_crypto_zec, "active", Gio.SettingsBindFlags.DEFAULT);
 	    }
     }
