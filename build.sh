d4734066acb10ea51be66a21ddbae569e13af505
sereneblue <sereneblue@users.noreply.github.com>
2023-09-02 23:50:14 -0400

    refactor: add build script for pre45/45 versions

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORK_DIR=`mktemp -d -p "$DIR"`
+
+function cleanup {      
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
+if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+
+if [[ $1 = "45" ]]; then
+	cp -r krypto@sereneblue/* $WORK_DIR
+	cp -r versions/45/* $WORK_DIR
+	pushd $WORK_DIR
+	mv ui/preferences_adw.ui ui/preferences.ui
+
+	sed -i -e 's/var/export const/g' ./constants.js
+	zip -r ../krypto_45.zip .
+	popd
+else 
+	cp -r krypto@sereneblue/* $WORK_DIR
+	cp -r versions/pre45/* $WORK_DIR
+	pushd $WORK_DIR
+	zip -r ../krypto_pre45.zip .
+	popd
+fi
+
+trap cleanup EXIT
